e1c9a6ec8baf51f1d9146a3af217ca404b31f553
Josey Tan <josey@live.com.sg>
2015-11-03 00:03:46 +0800

    Fixed session bug in process/setlocation.php
    Added extra condition on Line 6; to check the whether the page was
    directed from the page where user enters starting/pickup location.

--- hciproject/process/setlocation.php
+++ hciproject/process/setlocation.php
@@ -3,7 +3,7 @@ $prevLocation = basename($_SERVER['HTTP_REFERER']);
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['location']) && !empty($_POST["location"])){
-		if (!isset($_SESSION['start'])){
+		if (!isset($_SESSION['start']) || (isset($_SESSION['start']) && $prevLocation == 'location.php')){
 			$_SESSION['start'] = $_POST['location'];
 			header("location: ../setdestination.php");
 		}
